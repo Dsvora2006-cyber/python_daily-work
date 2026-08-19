cc9295f64465e6665b9577876a215061f04faea3
Dsvora2006-cyber <dsvora2006@gmail.com>
2026-08-19 06:23:33 +0530

    Add files via upload

--- workday_10.py
+++ workday_10.py
@@ -5,6 +5,11 @@ Created on Sat Aug 15 19:12:13 2026
 @author: darvo
 """
 
+"""The Delivery App Charge: A food delivery app charges differently based on distance,
+ whether it's raining, and whether the order value crosses a certain amount 
+ (which might waive the delivery fee entirely). How would you decide the order 
+ in which to check these conditions?"""
+
 normal_del=int(input("Input the normal delivery charges:-"))
 km=int(input("Enter the kelimeter:-"))
 km_charge=int(input("Enter the kelimeter charges:-"))
