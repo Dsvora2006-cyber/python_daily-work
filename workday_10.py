72ff08c40c2fe1a6c985cca9dada2903c3633586
Dsvora2006-cyber <dsvora2006@gmail.com>
2026-08-19 06:18:27 +0530

    Add files via upload

---
+++ workday_10.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 15 19:12:13 2026
+
+@author: darvo
+"""
+
+normal_del=int(input("Input the normal delivery charges:-"))
+km=int(input("Enter the kelimeter:-"))
+km_charge=int(input("Enter the kelimeter charges:-"))
+customer_cost=int(input("Enter the customer cost:-"))
+is_rain=input("The rain is heavily(yes/no):-")
+rain_charge=int(input("Enter the rain charges:-"))
+
+charges=0
+if customer_cost>=500 and  km<=5:
+    charges=0
+else:
+    charges=normal_del+(km*km_charge)+customer_cost
+if is_rain=="yes":
+    charges=normal_del+(km*km_charge)+rain_charge+customer_cost
+print("Total amount of delivery charges is:-",charges)
+
